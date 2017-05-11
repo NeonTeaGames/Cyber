af75616e2cbe19c189474ba7553ce6208f5b5c8b
excitedneon <excitedneon@gmail.com>
2017-05-11 03:40:00 +0300

    Rename BlinkyBox to Button, add doors, make them pink

--- Assets/Scripts/Entities/SyncBases/Button.cs
+++ Assets/Scripts/Entities/SyncBases/Button.cs
@@ -10,10 +10,15 @@ namespace Cyber.Entities.SyncBases {
     /// <summary>
     /// Object that blinks when interacted with.
     /// </summary>
-    public class BlinkyBox : Interactable {
+    public class Button : Interactable {
 
         /// <summary>
-        /// The lamp mesh that will blink.
+        /// The interactable which this button will trigger.
+        /// </summary>
+        public Interactable WillTrigger;
+
+        /// <summary>
+        /// The button mesh that will blink.
         /// </summary>
         public MeshRenderer Mesh;
 
@@ -32,42 +37,43 @@ namespace Cyber.Entities.SyncBases {
         /// </summary>
         public Color BlinkColor = new Color(1f, 0.6f, 0f);
 
-        private double BlinkTime = 0;
+        private float BlinkTime = float.MinValue;
         private Material Material;
 
         /// <summary>
-        /// When a BlinkyBox is interacted with, it blinks once, light lasting
-        /// for <see cref="BlinkLength"/> seconds.
+        /// When the button is interacted with, it blinks once, light lasting
+        /// for <see cref="BlinkLength"/> seconds, and calls Interact
+        /// on the <see cref="WillTrigger"/>.
         /// </summary>
         public override void Interact() {
-            BlinkTime = NetworkHelper.GetCurrentSystemTime();
-        }
-
-        /// <summary>
-        /// Update the blink time if the server has a newer blinktime
-        /// </summary>
-        /// <param name="reader"></param>
-        public override void Deserialize(NetworkReader reader) {
-            double ServerBlinkTime = reader.ReadDouble();
-            if (ServerBlinkTime > BlinkTime) {
-                BlinkTime = ServerBlinkTime;
+            BlinkTime = Time.time;
+            if (WillTrigger != null) {
+                WillTrigger.Interact();
+            } else {
+                Term.Println("FIXME: The button '" + gameObject.name + "' was pressed, but it doesn't have a WillTrigger.");
             }
         }
 
         /// <summary>
-        /// Write the blink time so others will sync if they have older blinks.
+        /// Buttons only act as triggers, so only interact is sent to the server.
         /// </summary>
-        /// <param name="writer"></param>
-        public override void Serialize(NetworkWriter writer) {
-            writer.Write(BlinkTime);
+        /// <param name="reader"></param>
+        public override void Deserialize(NetworkReader reader) {
         }
 
         /// <summary>
-        /// The blinky box doesn't need a hash and it should update every 5 ticks.
+        /// Buttons only act as triggers, so only interact is sent to the server.
+        /// </summary>
+        /// <param name="writer"></param>
+        public override void Serialize(NetworkWriter writer) {
+        }
+
+        /// <summary>
+        /// Buttons only act as triggers, so only interact is sent to the server.
         /// </summary>
         /// <returns>Sync Handletype containing sync information.</returns>
         public override SyncHandletype GetSyncHandletype() {
-            return new SyncHandletype(false, 5);
+            return new SyncHandletype(false, 1000);
         }
 
         private void Start() {
@@ -75,9 +81,9 @@ namespace Cyber.Entities.SyncBases {
         }
 
         private void Update() {
-            float Time = (float) (NetworkHelper.GetCurrentSystemTime() - BlinkTime);
-            if (Time < BlinkLength) {
-                float Brightness = (1f - Time / BlinkLength) * BlinkBrightness;
+            float CurrentTime = Time.time - BlinkTime;
+            if (CurrentTime < BlinkLength) {
+                float Brightness = (1f - CurrentTime / BlinkLength) * BlinkBrightness;
                 Color NewColor = new Color(Brightness * BlinkColor.r, 
                     Brightness * BlinkColor.g, Brightness * BlinkColor.b);
                 Material.SetColor("_EmissionColor", NewColor);
---
+++ Assets/Scripts/Entities/SyncBases/Door.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Cyber.Entities.SyncBases {
+    public class Door : Interactable {
+
+        /// <summary>
+        /// The root of the door mesh. This will be scaled to animate the door.
+        /// </summary>
+        public Transform DoorRoot;
+
+        /// <summary>
+        /// The openness of the door.
+        /// </summary>
+        public bool IsOpen = false;
+
+        /// <summary>
+        /// Toggles the openness of the door.
+        /// </summary>
+        public override void Interact() {
+            IsOpen = !IsOpen;
+        }
+
+        /// <summary>
+        /// Reads the openness of the door from the server.
+        /// </summary>
+        /// <param name="reader"></param>
+        public override void Deserialize(NetworkReader reader) {
+            IsOpen = reader.ReadBoolean();
+        }
+
+        /// <summary>
+        /// Writes the openness of the door.
+        /// </summary>
+        /// <param name="writer"></param>
+        public override void Serialize(NetworkWriter writer) {
+            writer.Write(IsOpen);
+        }
+
+        /// <summary>
+        /// Return the Sync Handletype information for <see cref="Syncer"/>.
+        /// </summary>
+        /// <returns>Sync Handletype containing sync information.</returns>
+        public override SyncHandletype GetSyncHandletype() {
+            return new SyncHandletype(false, 10);
+        }
+
+        private void Update() {
+            float DoorScale = IsOpen ? 0.01f : 1;
+            if (DoorRoot.localScale.x != DoorScale) {
+                Vector3 Scale = DoorRoot.localScale;
+                if (Mathf.Abs(Scale.x - DoorScale) < 0.01) {
+                    Scale.x = DoorScale;
+                } else {
+                    Scale.x = Mathf.Lerp(Scale.x, DoorScale, 5f * Time.deltaTime);
+                }
+                DoorRoot.localScale = Scale;
+            }
+        }
+    }
+}
